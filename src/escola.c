0bb97a1c4b778d22d16f94c659cea8d9e38902a7
Thiago Sanches <thiagomlkgame@gmail.com>
2024-10-24 16:00:21 -0300

    ATUALIZAÇÃO

---
+++ src/escola.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    float nt1, nt2, nt3, nt4, media;
+
+    // Recebendo as notaS
+    printf("Digite a nota1: \n");
+    scanf("%f", &nt1);
+     // Nota 2
+    printf("Digite a nota2: \n");
+    scanf("%f", &nt2);
+    // Nota 3
+    printf("Digite a nota3: \n");
+    scanf("%f", &nt3);
+    // Nota 4
+    printf("Digite a nota4: \n");
+    scanf("%f", &nt3);
+
+     // média
+    media = (nt1 + nt2 + nt3 + nt4) / 4;
+
+    // resultado
+    printf("A média das notas é: %.2f\n", media);
+
+    return 0;
+
+    }
+  
+   
+
+   
+
+
+
