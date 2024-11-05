66a0e7d26ded907e91515c9d9b762558c7c24d11
Thiago Sanches <thiagomlkgame@gmail.com>
2024-11-05 17:10:32 -0300

    atualização

---
+++ src/texto2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+
+    char nome[20];
+    printf("Coloque o primeiro nome seu e tecle Enter:\n");
+    scanf("%s",&nome);
+    printf("Olá, %s. Seja Bem-VIndo\n",nome);
+    return 0;
+}
