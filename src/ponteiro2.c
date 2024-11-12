6a01d4a00a4609a143c37b86fb8a8237a6f8b455
Thiago Sanches <thiagomlkgame@gmail.com>
2024-11-12 16:58:14 -0300

    conhecendo a varivel FILES e atualizando os ponteiros

--- src/ponteiro2.c
+++ src/ponteiro2.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
  /*void é uma função que não retorna*/
-void dados(int x, int y){
-    int soma = x + y;
-    printf("X está em %p e Y está em %p\n",&x,&y);
-     printf("Resultado é %d\n",soma);
+void dados(int *x, int *y){
+    int soma = *x + *y;
+    printf("X está em %p e Y está em %p \n",x,y);
+    printf("Resultado é %d\n",soma);
 }
 
 
 
 int main (){
-   
+
     int a = 10;
     int b = 20;
-    dados (a,b);
-    printf("A está em %p e B está em %p\n",&a,&b);
+    dados (&a,&b);
+    printf("A está em %p e b está em %p\n",&a,&b);
     
 
 
